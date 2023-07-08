2a62ae5c5014ea46b31a11005852c3cf588f865f
James Greiner <jamesdgreiner@icloud.com>
2023-07-07 18:41:27 -0700

    Initial commit with project files

---
+++ SwiftfulThinkingTodo/SwiftfulThinkingTodo/SwiftfulThinkingTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingTodoApp.swift
+//  SwiftfulThinkingTodo
+//
+//  Created by James Greiner on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
