c179be0e28f3b06beb314f5bc506f6fe3d2c4c8f
James Greiner <jamesdgreiner@icloud.com>
2023-07-07 22:42:38 -0700

    Update views to use item data model

--- SwiftfulThinkingTodo/SwiftfulThinkingTodo/Views/ListView.swift
+++ SwiftfulThinkingTodo/SwiftfulThinkingTodo/Views/ListView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct ListView: View {
-    @State var items: [String] = [
-        "This is the first title",
-        "This is the second title",
-        "This is the third title"
+    @State var items: [Item] = [
+        Item(title: "This is the first title", isCompleted: false),
+        Item(title: "This is the second title", isCompleted: true),
+        Item(title: "This is the third title", isCompleted: false),
     ]
     var body: some View {
         List {
-            ForEach(items, id: \.self) { item in
-                ListRowView(title: item)
+            ForEach(items) { item in
+                ListRowView(item: item)
             }
         }
         .listStyle(.plain)
