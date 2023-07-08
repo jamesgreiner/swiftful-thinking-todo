2e82cad5ee0f551a8195eed00264f03b892bb7e2
James Greiner <jamesdgreiner@icloud.com>
2023-07-07 19:35:37 -0700

    Add initial list view

--- SwiftfulThinkingTodo/SwiftfulThinkingTodo/SwiftfulThinkingTodoApp.swift
+++ SwiftfulThinkingTodo/SwiftfulThinkingTodo/SwiftfulThinkingTodoApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct SwiftfulThinkingTodoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ListView()
+            }
         }
     }
 }
